31fd5a5980e2c8489af34bd83d1713f7d2d8270a
Ilya Rezvov <ilya.rezvov@revjet.com>
2018-05-16 14:45:14 -0700

    use wasm builder for module generation

--- src/IRTS/CodegenWasm.hs
+++ src/IRTS/CodegenWasm.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DuplicateRecordFields #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE TypeFamilies #-}
 module IRTS.CodegenWasm (codegenWasm) where
 
 import Control.Monad.Reader (Reader, runReader, asks)
@@ -9,12 +12,15 @@ import Data.Word (Word8, Word16, Word32, Word64)
 import Data.Int (Int32, Int64)
 import Data.Bits ((.&.))
 import qualified Data.Char as Char
+import qualified Data.Map as Map
 import Data.Monoid ((<>), mempty)
+import Data.Proxy
+import Prelude hiding (and)
 
 import qualified Data.Serialize as Serialize
 import qualified Data.ByteString.Lazy as LBS
 import qualified Data.ByteString.Lazy.UTF8 as UTF8
-import qualified Data.ByteString.Builder as Builder
+import qualified Data.ByteString.Builder as BSBuilder
 
 import Idris.Core.TT (Name, Const(..), isTypeConst)
 import IRTS.Bytecode
@@ -22,6 +28,7 @@ import IRTS.CodegenCommon
 
 import Language.Wasm.Structure
 import qualified Language.Wasm.Binary as WasmBinary
+import Language.Wasm.Builder
 
 type TypeIndex = Natural
 
@@ -33,54 +40,88 @@ codegenWasm ci = do
 
 mkWasm :: [(Name, [BC])] -> Int -> Module
 mkWasm defs stackSize = 
-    let (funcs, st) = runWasmGen emptyState bindings $ mapM (mkFunc entryPointTypeIdx . snd) defs in
-    let GS { constSectionEnd, constSection } = st in
-    emptyModule {
-        mems = [
-            Memory $ Limit 10 Nothing
-        ],
-        datas = [
-            DataSegment 0 [I32Const 0] $ Builder.toLazyByteString constSection
-        ],
-        types = [
-            FuncType [I32] [] -- def type: (stackbase: i32) => void
-        ],
-        globals = [
-            Global (Const I32) [I32Const $ fromIntegral constSectionEnd], -- stack start
-            Global (Const I32) [I32Const $ constSectionEnd + fromIntegral stackSize], -- stack end
-            Global (Mut I32) [I32Const $ fromIntegral constSectionEnd], -- stack base
-            Global (Mut I32) [I32Const $ fromIntegral constSectionEnd], -- stack top
-            Global (Mut I32) [I32Const 0], -- return value
-            Global (Mut I32) [I32Const 0] -- tmp
-        ],
-        functions = funcs
-    }
-    where
-        entryPointTypeIdx = 0
-        bindings = GB {
-            stackStartIdx = 0,
-            stackEndIdx = 1,
-            stackBaseIdx = 2,
-            stackTopIdx = 3,
-            returnValueIdx = 4,
-            tmpIdx = 5
-        }
+    genMod $ do
+        gc <- importFunction "rts" "gc" (FuncType [I32] [])
+        memory 10 Nothing
+    
+        stackStart <- global Const i32 0
+        stackEnd <- global Const i32 0
+        stackBase <- global Mut i32 0
+        stackTop <- global Mut i32 0
+    
+        retReg <- global Mut i32 0
+        tmpReg <- global Mut i32 0
+    
+        heapStart <- global Mut i32 0
+        heapNext <- global Mut i32 0
+        heapEnd <- global Mut i32 0
+    
+        aligned <- fun $ do
+            size <- param i32
+            (size `add` i32c 3) `and` i32c 0xFFFFFFFC
+        alloc <- funRec $ \self -> do
+            size <- param i32
+            alignedSize <- local i32
+            addr <- local i32
+            alignedSize .= call i32 aligned [arg size]
+            ifExpr i32 ((heapNext `add` alignedSize) `lt_u` heapEnd)
+                (const $ do
+                    addr .= heapNext
+                    heapNext .= (heapNext `add` alignedSize)
+                    ret addr
+                )
+                (const $ do
+                    invoke gc []
+                    call i32 self [arg size]
+                )
+        reserve <- fun $ do
+            num <- param i32
+            i <- local i32
+            ifStmt (stackEnd `lt_u` (stackTop `add` (num `mul` i32c 4)))
+                (const unreachable)
+                (const $ for (i .= i32c 0) (i `lt_u` num) (i .= (i `add` i32c 1)) $ const $ do
+                    store (stackTop `add` (i `mul` i32c 4)) (i32c 0) 0 0
+                )
+        defsStartFrom <- nextFuncIndex
+        let bindings = GB {
+                stackStartIdx = stackStart,
+                stackEndIdx = stackEnd,
+                stackBaseIdx = stackBase,
+                stackTopIdx = stackTop,
+                returnValueIdx = retReg,
+                tmpIdx = tmpReg,
+                allocFn = alloc,
+                reserveFn = reserve,
+                symbols = Map.fromList $ zipWith (,) (map fst defs) [defsStartFrom..]
+            }
+        let (funcs, st) = runWasmGen emptyState bindings $ mapM (mkFunc . snd) defs
+        let GS { constSectionEnd, constSection } = st
+        sequence_ funcs
+        setGlobalInitializer stackStart $ fromIntegral constSectionEnd
+        setGlobalInitializer stackEnd $ fromIntegral constSectionEnd + fromIntegral stackSize
+        setGlobalInitializer stackBase $ fromIntegral constSectionEnd
+        setGlobalInitializer stackTop $ fromIntegral constSectionEnd
+        dataSegment (i32c 0) $ BSBuilder.toLazyByteString constSection
+        return ()
 
 data GenState = GS {
     constSectionEnd :: Word32,
-    constSection :: Builder.Builder
+    constSection :: BSBuilder.Builder
 }
 
 emptyState :: GenState
 emptyState = GS 0 mempty
 
 data GlobalBindings = GB {
-    stackBaseIdx :: GlobalIndex,
-    stackTopIdx :: GlobalIndex,
-    stackStartIdx :: GlobalIndex,
-    stackEndIdx :: GlobalIndex,
-    returnValueIdx :: GlobalIndex,
-    tmpIdx :: GlobalIndex
+    stackBaseIdx :: Glob I32,
+    stackTopIdx :: Glob I32,
+    stackStartIdx :: Glob I32,
+    stackEndIdx :: Glob I32,
+    returnValueIdx :: Glob I32,
+    tmpIdx :: Glob I32,
+    symbols :: Map.Map Name Natural,
+    reserveFn :: Natural,
+    allocFn :: Natural
 }
 
 type WasmGen = StateT GenState (Reader GlobalBindings)
@@ -88,50 +129,64 @@ type WasmGen = StateT GenState (Reader GlobalBindings)
 runWasmGen :: GenState -> GlobalBindings -> WasmGen a -> (a, GenState)
 runWasmGen st bindings = flip runReader bindings . flip runStateT st
 
-mkFunc :: TypeIndex -> [BC] -> WasmGen Function
-mkFunc typeIdx byteCode = Function typeIdx [I32] <$> concat <$> mapM bcToInstr byteCode
+mkFunc :: [BC] -> WasmGen (GenMod Natural)
+mkFunc byteCode = do
+    body <- mapM bcToInstr byteCode
+    return $ fun $ do
+        oldBase <- param i32
+        myOldBase <- local i32
+        sequence_ $ map ($ (oldBase, myOldBase)) body
 
-bcToInstr :: BC -> WasmGen [Instruction]
-bcToInstr (ASSIGN l r) = getRegVal r >>= setRegVal l
-bcToInstr (ASSIGNCONST reg c) = makeConst c >>= setRegVal reg
-bcToInstr (UPDATE l r) = getRegVal r >>= setRegVal l
-bcToInstr _ = return []
-
-getRegVal :: Reg -> WasmGen [Instruction]
+bcToInstr :: BC -> WasmGen ((Loc I32, Loc I32) -> GenFun ())
+bcToInstr (ASSIGN l r) = const <$> (getRegVal r >>= setRegVal l)
+bcToInstr (ASSIGNCONST reg c) = const <$> (makeConst c >>= setRegVal reg)
+bcToInstr (UPDATE l r) = const <$> (getRegVal r >>= setRegVal l)
+bcToInstr (MKCON l _ tag args) = const <$> setRegVal l (i32c 0)
+bcToInstr (RESERVE 0) = return $ const $ return ()
+bcToInstr (RESERVE n) = do
+    reserve <- asks reserveFn
+    return $ const $ invoke reserve [i32c n]
+bcToInstr STOREOLD = do
+    stackBase <- asks stackBaseIdx
+    return $ \(_, myOldBase) -> myOldBase .= stackBase
+bcToInstr _ = return $ const $ return ()
+    
+getRegVal :: Reg -> WasmGen (GenFun (Proxy I32))
 getRegVal RVal = do
     idx <- asks returnValueIdx
-    return [GetGlobal idx]
+    return $ produce idx
 getRegVal Tmp = do
     idx <- asks tmpIdx
-    return [GetGlobal idx]
+    return $ produce idx
 getRegVal (L offset) = do
     idx <- asks stackBaseIdx
-    return [GetGlobal idx, I32Load (MemArg (fromIntegral offset * 4) 2)]
+    return $ load i32 idx (offset * 4) 2
 getRegVal (T offset) = do
     idx <- asks stackTopIdx
-    return [GetGlobal idx, I32Load (MemArg (fromIntegral offset * 4) 2)]
+    return $ load i32 idx (offset * 4) 2
 
-setRegVal :: Reg -> [Instruction] -> WasmGen [Instruction]
+setRegVal :: (Producer expr, OutType expr ~ Proxy I32) => Reg -> expr -> WasmGen (GenFun ())
 setRegVal RVal val = do
     idx <- asks returnValueIdx
-    return $ val ++ [SetGlobal idx]
+    return $ idx .= val
 setRegVal Tmp val = do
     idx <- asks tmpIdx
-    return $ val ++ [SetGlobal idx]
+    return $ idx .= val
 setRegVal (L offset) val = do
     idx <- asks stackBaseIdx
-    return $ [GetGlobal idx] ++ val ++ [I32Store (MemArg (fromIntegral offset * 4) 2)]
+    return $ store idx val (offset * 4) 2
 setRegVal (T offset) val = do
     idx <- asks stackTopIdx
-    return $ [GetGlobal idx] ++ val ++ [I32Store (MemArg (fromIntegral offset * 4) 2)]
+    return $ store idx val (offset * 4) 2
 
-asAddr :: WasmGen Word32 -> WasmGen [Instruction]
+asAddr :: WasmGen Word32 -> WasmGen (GenFun (Proxy I32))
 asAddr expr = do
     addr <- expr
-    return [I32Const addr]
+    return $ i32c addr
 
-makeConst :: Const -> WasmGen [Instruction]
+makeConst :: Const -> WasmGen (GenFun (Proxy I32))
 makeConst (I i) = asAddr $ addToConstSection (mkInt i)
+makeConst (BI i) = asAddr $ addToConstSection (mkBigInt i)
 makeConst (Fl f) = asAddr $ addToConstSection (mkFloat f)
 makeConst (Ch c) = asAddr $ addToConstSection (mkInt $ Char.ord c)
 makeConst (Str s) = asAddr $ addToConstSection (mkStr s)
@@ -153,15 +208,17 @@ addToConstSection val = do
     let sz = fromIntegral $ LBS.length bytes
     let asz = aligned sz
     -- alignment gap
-    let gap = Builder.lazyByteString $ LBS.replicate (fromIntegral $ asz - sz) 0
-    put $ GS (addr + asz) (builder <> Builder.lazyByteString bytes <> gap)
+    let gap = BSBuilder.lazyByteString $ LBS.replicate (fromIntegral $ asz - sz) 0
+    put $ GS (addr + asz) (builder <> BSBuilder.lazyByteString bytes <> gap)
     return addr
 
 data ValType
-    = Int
+    = Con
+    | Int
     | BigInt
     | Float
     | String
+    | StrOffset
     | Bit32
     | Bit64
     deriving (Eq, Show, Enum)
@@ -254,6 +311,36 @@ instance Serialize.Serialize Bit64Val where
         Serialize.put hdr
         Serialize.putWord64le val
     get = B64V <$> Serialize.get <*> Serialize.getWord64le
+
+-- TODO: emulate Big Nums as i64 for now. Add runtime support for real big numbers
+data BigIntVal = BIV { hdr :: ValHeader, val :: Word64 } deriving (Show, Eq)
+
+mkBigInt :: Integer -> BigIntVal
+mkBigInt val = BIV { hdr = mkHdr Int 16, val = fromIntegral val }
+
+instance Serialize.Serialize BigIntVal where
+    put BIV { hdr, val } = do
+        Serialize.put hdr
+        Serialize.putWord64le val
+    get = BIV <$> Serialize.get <*> Serialize.getWord64le
+
+data ConVal = CV { hdr :: ValHeader, tag :: Word32, args :: [Word32] } deriving (Show, Eq)
+
+mkCon :: Word32 -> [Word32] -> ConVal
+mkCon tag args =
+    let header = mkHdr Con (8 + 4 + 4 * length args) in
+    CV { hdr = header { slot16 = fromIntegral $ length args }, tag, args }
+
+instance Serialize.Serialize ConVal where
+    put CV { hdr, tag, args } = do
+        Serialize.put hdr
+        Serialize.putWord32le tag
+        mapM_ Serialize.putWord32le args
+    get = do
+        hdr <- Serialize.get
+        tag <- Serialize.getWord32le
+        args <- sequence $ replicate (fromIntegral $ slot16 hdr) Serialize.getWord32le
+        return $ CV { hdr, tag, args }
 
 asWord32 :: Int32 -> Word32
 asWord32 i
